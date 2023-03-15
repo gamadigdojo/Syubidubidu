58c34e4f2a3ceb6caf413e139216e82426ee62d0
JonathanSamuelTan <jonathansam215@gmail.com>
2023-03-15 20:15:09 +0700

    Sam : Login Done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -44,4 +44,45 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('CSS/Login.css')}}">
+    <title>login</title>
+</head>
+<body>
+    <div class="form">
+        <div class="container">
+            <div class="title">
+                <h1>Ido Store</h1>
+            </div>
+            <form method="POST" action="{{ route('login') }}" onsubmit="return validateForm()">
+                @csrf
+                <div class="input-box">
+                    <input type="text" name="email" id="email" placeholder="Email" name='email'>
+                    <div class="error-message" id="error-email"></div>
+                </div>
+                <div class="input-box">
+                    <input type="password" name="password" id="password" placeholder="Password" name='password'>
+                    <div class="error-message" id="error-pass"></div>
+                </div>
+                <div class="remember-me">
+                    <input type="checkbox" name="checkbox" id="checkbox" class="checkbox" name='remember'>
+                    <div class="description">Remember me</div>
+                </div>
+                <div class="btn">
+                    <button class="submitBtn" type="submit">Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
+   
+</body>
+<script src="{{asset('JS/Login.js')}}"></script>
+</html>
+
